bbe2ad9fc7bd155ff8a20835813a83ef0ddc4a53
Salman Khan <skhan231714@gmail.com>
2023-05-22 01:24:12 +0500

    edit frontend ui

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    goals: goalReducer,
   },
-})
+});
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,101 +1,50 @@
-import { useState, useEffect } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { login, reset } from '../features/auth/authSlice'
-import Spinner from '../components/Spinner'
+import { useState,useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
 
 function Login() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
+    const [formData,setFormData] = useState({
+        email:'',
+        password:'',
+    })
 
-  const { email, password } = formData
-
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  )
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(message)
+    const {name,email,password,password2} = formData;
+    const onChange=(e)=>{
+        setFormData((prevState)=>({
+            ...prevState,[e.target.name]:e.target.value,
+        }))
     }
+    const onSubmit=(e)=>{ 
+        e.prevenDefault()
+     }
 
-    if (isSuccess || user) {
-      navigate('/')
-    }
-
-    dispatch(reset())
-  }, [user, isError, isSuccess, message, navigate, dispatch])
-
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    const userData = {
-      email,
-      password,
-    }
-
-    dispatch(login(userData))
-  }
-
-  if (isLoading) {
-    return <Spinner />
-  }
-
-  return (
+    return (
     <>
-      <section className='heading'>
+     <section className="heading">
         <h1>
-          <FaSignInAlt /> Login
+            <FaSignInAlt/> Login
         </h1>
-        <p>Login and start setting goals</p>
-      </section>
+        <p>Login and Start setting goals</p>
+        </section>   
 
-      <section className='form'>
-        <form onSubmit={onSubmit}>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Enter your email'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
-            />
-          </div>
+        <section className='form'>
+            <form onSubmit={onSubmit}>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
+            <div className="form-group">
+                <input type="text" className='form-control' id='email' name='email' value={email} placeholder='Enter Your Email' 
+                onChange={onChange}/>
+            </div>
+
+            <div className="form-group">
+                <input type="text" className='form-control' id='password' name='password' value={password} placeholder='Enter Password' 
+                onChange={onChange}/>
+            </div>
+
+        <div className="form-group">
+            <button type="submit" className='btn btn-block'>Submit</button>
+        </div>
+            </form>
+        </section>
+
     </>
   )
 }
